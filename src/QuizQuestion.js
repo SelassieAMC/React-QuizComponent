d2ed32b973492246af71dd85d40676f73b1511fc
Andres Mauricio Clavijo Mejia <aclavijo@belatrix.com>
2020-05-12 14:10:11 -0500

    Displaying an error message based on state

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,12 +8,11 @@ class QuizQuestion extends Component{
     }
 
     handleClick(button_text){
-        button_text === this.props.quiz_question.answer ? 
-            this.setState({incorrectAnswer: true}):
-            this.setState({incorrectAnswer: false});
-        
-        if(this.state.incorrectAnswer){
-            this.props.showNextQuestionHandler();
+        if(buttonText === this.props.quiz_question.answer) {
+        this.setState({ incorrectAnswer: false })
+        this.props.showNextQuestionHandler()
+        } else {
+        this.setState({ incorrectAnswer: true })
         }
     }
     render(){
